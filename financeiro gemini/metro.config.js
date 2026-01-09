fdf985b5f1bd8cec8e2abcb7e7305705064ab851
GabrielSanHub <gabriel.rochabarros@hotmail.com>
2026-01-09 08:33:06 -0300

    removed copy folder

--- financeiro gemini/metro.config.js
+++
@@ -1,9 +0,0 @@
-// financeiro/metro.config.js
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Adiciona 'wasm' como uma extensão de asset (arquivo estático)
-config.resolver.assetExts.push('wasm');
-
-module.exports = config;
